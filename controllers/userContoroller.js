1c17c19f3667daed517426138e8678431cd1bce8
XasanAbdi <xasancabdinasax@gmail.com>
2023-07-13 07:07:28 -0700

    first commit

--- controllers/userContoroller.js
+++ controllers/userContoroller.js
@@ -4,12 +4,12 @@ import generateToken from "../utilits/generateToken.js";
 
 export const register=async(req,res)=>{
     const {name,email,password,address,phone}=req.body;
-    const userExists=Users.findOne({email})
-    if (userExists) {
-        res.status(400).json({message:'user a;;ready Exist'})
+    // const userExists=Users.findOne({email})
+    // if (userExists) {
+    //     res.status(400).json({message:'user allready Exist'})
         
-    }
-    else{
+    // }
+    
         const user=Users.create({
             name,email,password,address,phone
         })
@@ -27,7 +27,7 @@ export const register=async(req,res)=>{
         }else{
             res.status(401).json({message:'invalid data'});
         }
-    }
+    
 }
 
 export const login=async(req,res)=>{
